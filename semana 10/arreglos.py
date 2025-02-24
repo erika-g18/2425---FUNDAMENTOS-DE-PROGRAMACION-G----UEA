ba49025fa1695deb7aff92730fbfa6a7f3324e08
erika-g18 <em.guamana@uea.edu.ec>
2025-02-23 22:14:42 -0500

    Add files via upload

---
+++ semana 10/arreglos.py
@@ -0,0 +1,14 @@
+# semana 10 arreglos
+
+matriz=[
+    [1, 24, 15],
+    [4, 50, 5.5],
+    ["juan", "sofi"],
+    [3, 8, 10]
+]
+print(matriz)
+print(matriz[2])
+print(matriz[1][2])
+matriz[3][1]=23
+
+print(matriz)
